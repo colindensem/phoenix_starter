09f53b307bfe76b8b36080a35593a8beef31f828
Colin Densem <colin@summit360.co.uk>
2016-12-24 16:05:51 +0000

    Remove Credo check from test - duplicated in CI

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule PhoenixStarter.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "credo", "test"]
+     "test": ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
